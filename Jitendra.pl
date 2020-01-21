ac22b2708c44f1211dc17748843a964df82f1adb
gk235329 <52865530+gk235329@users.noreply.github.com>
2020-01-21 16:00:52 +0530

    Add files via upload

---
+++ Jitendra.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+#if -elseif -else
+$word = 'MNIDDKL';
+if($word eq 'QSTVSGE')
+{
+  print "QSTVGE\n";
+}
+elsif($word eq 'MRQQDMISHDEL')
+{
+  print "MRQQDMISHDEL\n";
+}
+elsif($word eq 'MNIDDKL')
+{
+  print "MNIDDKL\n";
+}
+else
+{
+  print "NOT SURE\n";
+}
+exit

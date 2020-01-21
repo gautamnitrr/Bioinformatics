072f7d2a85f44885f69d154eb28008770c152737
gk235329 <52865530+gk235329@users.noreply.github.com>
2020-01-21 16:26:56 +0530

    Add files via upload

---
+++ Splice.pl
@@ -0,0 +1,10 @@
+#shift operation on arrays
+#!/usr/bin/perl -w
+
+@bases = ('A','C','D','T');
+splice(@bases,2,0,'X');
+print $base2, "\n\n";
+print "Here's the array with an element imserted after the 2nd element:";
+print "@bases\n";
+print "=========";
+print "\n\n";
